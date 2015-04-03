1e05e860d983b77272ec0e01786e8e0ffa59a5eb
Wai Ho Leung <whleung@live.com>
2015-04-03 14:37:41 -0700

    Added player and keyboard event handler.

--- TempoClient/src/main.cpp
+++ TempoClient/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Windows.h>
 
+#include <osg/Shape>
+#include <osg/ShapeDrawable>
 #include <osgViewer/Viewer>
+#include <osgGA/TrackballManipulator>
+
+#include "KeyboardEventHandler.h"
 
 int main() {
     osgViewer::Viewer viewer;
+
+	osg::ref_ptr<osg::Shape> player(new osg::Box);
+	osg::ref_ptr<osg::ShapeDrawable> playerDrawable(new osg::ShapeDrawable(player));
+
+	osg::ref_ptr<osg::Geode> playerNode(new osg::Geode);
+	playerNode->addDrawable(playerDrawable);
+
+	viewer.setSceneData(playerNode);
+	viewer.setCameraManipulator(new osgGA::TrackballManipulator);
+	viewer.addEventHandler(new KeyboardEventHandler);
 
     viewer.realize();
 
