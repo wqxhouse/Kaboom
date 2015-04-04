7f6f8800267daca90bbf253d7a92900c4dd40e3a
Wai Ho Leung <whleung@live.com>
2015-04-03 17:04:10 -0700

    Added support for multiple key presses.

--- TempoClient/src/KeyboardEventHandler.cpp
+++ TempoClient/src/KeyboardEventHandler.cpp
@@ -3,25 +3,72 @@
 #include "Global.h"
 #include "KeyboardEventHandler.h"
 
-bool KeyboardEventHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa) {
-	switch (ea.getEventType()) {
-	case osgGA::GUIEventAdapter::KEYDOWN:
-		switch (ea.getKey()) {
-		case 'w':
-			position.y() += 0.05;
-			return false;
-		case 's':
-			position.y() -= 0.05;
-			return false;
-		case 'a':
-			position.x() -= 0.05;
-			return false;
-		case 'd':
-			position.x() += 0.05;
-			return false;
-		default:
+bool KeyboardEventHandler::registerKey(int key, Function func) {
+	if (keyDownFuncMap.end() != keyDownFuncMap.find(key)) {
+		std::cout << "Duplicate key '" << key << "' ignored." << std::endl;
+		return false;
+	}
+	else {
+		keyDownFuncMap[key].keyFunction = func;
+		return true;
+	}
+}
+
+bool KeyboardEventHandler::registerKey(int key, KeyState state, Function func) {
+	if (state == KEY_DOWN) {
+		return registerKey(key, func);
+	}
+	else {
+		if (keyUpFuncMap.end() != keyUpFuncMap.find(key)) {
+			std::cout << "Duplicate key '" << key << "' ignored." << std::endl;
 			return false;
 		}
+		else {
+			keyUpFuncMap[key].keyFunction = func;
+			return true;
+		}
+	}
+}
+
+bool KeyboardEventHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa) {
+	KeyFunctionMap::iterator itr;
+	bool newKeyDownEvent = false;
+	bool newKeyUpEvent = false;
+
+	switch (ea.getEventType()) {
+	case osgGA::GUIEventAdapter::KEYDOWN:
+		itr = keyDownFuncMap.find(ea.getKey());
+
+		if (itr != keyDownFuncMap.end()) {
+			if (itr->second.keyState == KEY_UP) {
+				itr->second.keyState = KEY_DOWN;
+				newKeyDownEvent = true;
+			}
+
+			if (newKeyDownEvent) {
+				itr->second.keyFunction();
+				newKeyDownEvent = false;
+			}
+
+			return true;
+		}
+
+		return false;
+	case osgGA::GUIEventAdapter::KEYUP:
+		itr = keyDownFuncMap.find(ea.getKey());
+
+		if (itr != keyDownFuncMap.end()) {
+			itr->second.keyState = KEY_UP;
+		}
+
+		itr = keyUpFuncMap.find(ea.getKey());
+
+		if (itr != keyUpFuncMap.end()) {
+			itr->second.keyFunction();
+			return true;
+		}
+
+		return false;
 	default:
 		return false;
 	}
--- TempoClient/src/KeyboardEventHandler.h
+++ TempoClient/src/KeyboardEventHandler.h
@@ -6,6 +6,32 @@
 
 class KeyboardEventHandler : public osgGA::GUIEventHandler {
 public:
+	typedef void(*Function) ();
+
+	enum KeyState {
+		KEY_UP,
+		KEY_DOWN
+	};
+
+	struct FunctionState {
+		FunctionState() {
+			keyState = KEY_UP;
+			keyFunction = NULL;
+		}
+
+		Function keyFunction;
+		KeyState keyState;
+	};
+
+	typedef std::map<int, FunctionState> KeyFunctionMap;
+
+	bool registerKey(int key, Function func);
+	bool registerKey(int key, KeyState state, Function func);
+
 	virtual bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa);
 	virtual void accept(osgGA::GUIEventHandlerVisitor& v);
+
+protected:
+	KeyFunctionMap keyDownFuncMap;
+	KeyFunctionMap keyUpFuncMap;
 };
