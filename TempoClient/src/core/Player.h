a52834cd396b2e8f2e8fc7a37d8f12a9574b312b
Wai Ho Leung <whleung@live.com>
2015-04-06 19:53:52 -0700

    Changed tabs to spaces.

--- TempoClient/src/core/Player.h
+++ TempoClient/src/core/Player.h
@@ -4,26 +4,26 @@
 
 class Player {
 public:
-	static void moveForwardDown();
-	static void moveForwardUp();
-	static void moveBackwardDown();
-	static void moveBackwardUp();
-	static void moveLeftDown();
-	static void moveLeftUp();
-	static void moveRightDown();
-	static void moveRightUp();
-	static void jumpDown();
-	static void jumpUp();
+    static void moveForwardDown();
+    static void moveForwardUp();
+    static void moveBackwardDown();
+    static void moveBackwardUp();
+    static void moveLeftDown();
+    static void moveLeftUp();
+    static void moveRightDown();
+    static void moveRightUp();
+    static void jumpDown();
+    static void jumpUp();
 
-	bool movingForward;
-	bool movingBackward;
-	bool movingLeft;
-	bool movingRight;
-	bool jumping;
+    bool movingForward;
+    bool movingBackward;
+    bool movingLeft;
+    bool movingRight;
+    bool jumping;
 
-	osg::Vec3 position;
-	osg::Vec3 velocity;
+    osg::Vec3 position;
+    osg::Vec3 velocity;
 
-	Player();
-	~Player();
+    Player();
+    ~Player();
 };
