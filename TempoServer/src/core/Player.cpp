941cb094d592efd26d8142402573d6c1593ae14a
Wai Ho Leung <whleung@live.com>
2015-04-08 16:51:00 -0700

    Added partial implementation for player physics.

--- TempoServer/src/core/Player.cpp
+++ TempoServer/src/core/Player.cpp
@@ -1,7 +1,13 @@
 #include "Player.h"
 
-Player::Player() {
+btCollisionShape *Player::collisionShape = new btBoxShape(btVector3(0.5f, 0.5f, 0.5f));
+
+Player::Player(PlayerData *data) : data(data) {
 }
 
 Player::~Player() {
+}
+
+void Player::update(btScalar timeStep) {
+
 }
