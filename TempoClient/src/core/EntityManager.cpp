999f77657849233a5aef121446eba8eafd4ea2c6
Wai Ho Leung <whleung@live.com>
2015-04-10 14:42:05 -0700

    Added more methods to EntityManager.

--- TempoClient/src/core/EntityManager.cpp
+++ TempoClient/src/core/EntityManager.cpp
@@ -7,19 +7,40 @@ EntityManager::~EntityManager() {
 }
 
 Entity *EntityManager::createEntity() {
-    while (entities.count(nextId)) {
+    while (isEntityAlive(nextId)) {
         ++nextId;
     }
 
-    Entity *entity = new Entity(nextId);
+    return createEntity(nextId++);
+}
 
-    entities[nextId] = entity;
-    ++nextId;
+Entity *EntityManager::createEntity(unsigned int id) {
+    if (isEntityAlive(id)) {
+        return NULL;
+    }
+
+    Entity *entity = new Entity(id);
+
+    entities[id] = entity;
 
     return entity;
 }
 
 void EntityManager::destroyEntity(unsigned int id) {
-    delete entities[id];
-    entities.erase(id);
+    if (isEntityAlive(id)) {
+        delete entities[id];
+        entities.erase(id);
+    }
+}
+
+Entity *EntityManager::getEntity(unsigned int id) {
+    if (isEntityAlive(id)) {
+        return entities[id];
+    } else {
+        return NULL;
+    }
+}
+
+bool EntityManager::isEntityAlive(unsigned int id) {
+    return entities.count(id) > 0;
 }
--- TempoClient/src/core/EntityManager.h
+++ TempoClient/src/core/EntityManager.h
@@ -10,7 +10,12 @@ public:
     ~EntityManager();
 
     Entity *createEntity();
+    Entity *createEntity(unsigned int id);
+
     void destroyEntity(unsigned int id);
+
+    Entity *getEntity(unsigned int id);
+    bool isEntityAlive(unsigned int id);
 
 private:
     std::unordered_map<unsigned int, Entity *> entities;
