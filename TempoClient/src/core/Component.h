e2b1095e7cfc5f2e30f7d44a93b7cac7cedd77c8
Wai Ho Leung <whleung@live.com>
2015-04-12 02:25:50 -0700

    Added update functions to entity and component.

--- TempoClient/src/core/Component.h
+++ TempoClient/src/core/Component.h
@@ -1,6 +1,19 @@
 #pragma once
 
+#include "Entity.h"
+#include "GameStateData.h"
+
+class Entity;
+
 class Component {
 public:
     virtual ~Component() {}
+
+    virtual void update(const GameStateData &data);
+
+    Entity *getEntity() const;
+    void setEntity(Entity *entity);
+
+protected:
+    Entity *entity;
 };
--- TempoClient/src/core/Entity.cpp
+++ TempoClient/src/core/Entity.cpp
@@ -8,7 +8,14 @@ Entity::~Entity() {
 }
 
 void Entity::attachComponent(Component *component) {
+    component->setEntity(this);
     components[&typeid(*component)] = component;
+}
+
+void Entity::update(const GameStateData &data) {
+    for (auto it : components) {
+        it.second->update(data);
+    }
 }
 
 unsigned int Entity::getId() const {
