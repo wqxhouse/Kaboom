1801efebd79069aec211e2df138b5c40b6c3f0cc
Wai Ho Leung <whleung@live.com>
2015-04-16 13:06:30 -0700

    Overloaded stream operator in PlayerInputEvent for debugging purposes.

--- KaboomEngine/src/network/PlayerInputEvent.h
+++ KaboomEngine/src/network/PlayerInputEvent.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <ostream>
+
 #include "Event.h"
 
 class PlayerInputEvent : public Event {
 public:
-	PlayerInputEvent();
+    PlayerInputEvent();
     PlayerInputEvent(unsigned int playerId,
         bool movingForward,
         bool movingBackward,
@@ -32,6 +34,22 @@ public:
 
     virtual void serialize(char *buf) const;
     virtual void deserialize(char *buf);
+
+    friend std::ostream& operator<<(std::ostream &os, const PlayerInputEvent &o) {
+        os << "PlayerInputEvent: {" << std::endl;
+        os << "    playerId: " << o.playerId << std::endl;
+        os << "    movingForward: " << o.movingForward << std::endl;
+        os << "    movingBackward: " << o.movingBackward << std::endl;
+        os << "    movingLeft: " << o.movingLeft << std::endl;
+        os << "    movingRight: " << o.movingRight << std::endl;
+        os << "    jumping: " << o.jumping << std::endl;
+        os << "    firing: " << o.firing << std::endl;
+        os << "    yaw: " << o.yaw << std::endl;
+        os << "    pitch: " << o.pitch << std::endl;
+        os << "}";
+
+        return os;
+    }
 
 private:
     unsigned int playerId;
