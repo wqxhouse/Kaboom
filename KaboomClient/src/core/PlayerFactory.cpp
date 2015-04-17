4c63113d1fe5a5c31d9212cec8c5c5cc50d0de78
Wai Ho Leung <whleung@live.com>
2015-04-17 02:43:29 -0700

    Removed ClientPlayer since we could access components directly.

--- KaboomClient/src/core/PlayerFactory.cpp
+++ KaboomClient/src/core/PlayerFactory.cpp
@@ -18,23 +18,9 @@ PlayerFactory::PlayerFactory(EntityManager *entityManager)
 PlayerFactory::~PlayerFactory() {
 }
 
-ClientPlayer *PlayerFactory::createPlayer() const {
-    createPlayer(0.0f, 0.0f, 0.0f);
-}
-
-ClientPlayer *PlayerFactory::createPlayer(float x, float y, float z) const {
-    Entity *entity = entityManager->createEntity();
-
-    return createPlayer(entity, x, y, z);
-}
-
-ClientPlayer *PlayerFactory::createPlayer(unsigned int id, float x, float y, float z) const {
+Entity *PlayerFactory::createPlayer(unsigned int id, float x, float y, float z) const {
     Entity *entity = entityManager->createEntity(id);
 
-    return createPlayer(entity, x, y, z);
-}
-
-ClientPlayer *PlayerFactory::createPlayer(Entity *entity, float x, float y, float z) const {
     osg::Box *box = new osg::Box;
     osg::ShapeDrawable *drawable = new osg::ShapeDrawable(box);
     osg::Geode *model = new osg::Geode;
@@ -50,5 +36,5 @@ ClientPlayer *PlayerFactory::createPlayer(Entity *entity, float x, float y, floa
     entity->attachComponent(new SceneNodeComponent(playerNode));
     entity->attachComponent(new PositionComponent(x, y, z));
 
-    return static_cast<ClientPlayer *>(entity);
+    return entity;
 }
