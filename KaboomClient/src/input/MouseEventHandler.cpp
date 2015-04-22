d25648a5b2cd4372f1a011ae4f35fdfc9d09e1c2
Wai Ho Leung <whleung@live.com>
2015-04-21 21:28:07 -0700

    Implemented MouseEventHandler.

--- KaboomClient/src/input/MouseEventHandler.cpp
+++ KaboomClient/src/input/MouseEventHandler.cpp
@@ -9,7 +9,35 @@ MouseEventHandler::MouseEventHandler(InputEventHandler &inputEventHandler)
 MouseEventHandler::~MouseEventHandler() {
 }
 
+bool MouseEventHandler::bindKey(int key, Function func) {
+    if (keyDownFuncMap.end() != keyDownFuncMap.find(key)) {
+        std::cout << "Duplicate key '" << key << "' ignored." << std::endl;
+        return false;
+    } else {
+        keyDownFuncMap[key].keyFunction = func;
+        return true;
+    }
+}
+
+bool MouseEventHandler::bindKey(int key, KeyState state, Function func) {
+    if (state == KEY_DOWN) {
+        return bindKey(key, func);
+    } else {
+        if (keyUpFuncMap.end() != keyUpFuncMap.find(key)) {
+            std::cout << "Duplicate key '" << key << "' ignored." << std::endl;
+            return false;
+        } else {
+            keyUpFuncMap[key].keyFunction = func;
+            return true;
+        }
+    }
+}
+
 bool MouseEventHandler::handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &aa) {
+    KeyFunctionMap::iterator itr;
+    bool newKeyDownEvent = false;
+    bool newKeyUpEvent = false;
+
     int centerX = (ea.getWindowX() + ea.getWindowWidth()) / 2;
     int centerY = (ea.getWindowY() + ea.getWindowHeight()) / 2;
     int x = ea.getX();
@@ -22,8 +50,37 @@ bool MouseEventHandler::handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIActio
 
     switch (ea.getEventType()) {
     case osgGA::GUIEventAdapter::KEYDOWN:
+        itr = keyDownFuncMap.find(ea.getKey());
+
+        if (itr != keyDownFuncMap.end()) {
+            if (itr->second.keyState == KEY_UP) {
+                itr->second.keyState = KEY_DOWN;
+                newKeyDownEvent = true;
+            }
+
+            if (newKeyDownEvent) {
+                (inputEventHandler.*(itr->second.keyFunction))();
+                newKeyDownEvent = false;
+            }
+
+            return true;
+        }
+
         return false;
     case osgGA::GUIEventAdapter::KEYUP:
+        itr = keyDownFuncMap.find(ea.getKey());
+
+        if (itr != keyDownFuncMap.end()) {
+            itr->second.keyState = KEY_UP;
+        }
+
+        itr = keyUpFuncMap.find(ea.getKey());
+
+        if (itr != keyUpFuncMap.end()) {
+            (inputEventHandler.*(itr->second.keyFunction))();
+            return true;
+        }
+
         return false;
     case osgGA::GUIEventAdapter::MOVE:
         if (dx != 0 || dy != 0) {
