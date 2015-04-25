c63632707c7dce2f77b995ac107092d5bb3deab6
Wai Ho Leung <whleung@live.com>
2015-04-24 23:20:15 -0700

    Fixed null pointer error while getting RotationComponent on client side.

--- KaboomClient/src/core/PlayerFactory.cpp
+++ KaboomClient/src/core/PlayerFactory.cpp
@@ -7,6 +7,7 @@
 
 #include <core/EntityManager.h>
 #include <core/PositionComponent.h>
+#include <core/RotationComponent.h>
 
 #include "SceneNodeComponent.h"
 
@@ -34,6 +35,7 @@ Entity *PlayerFactory::createPlayer(unsigned int id, float x, float y, float z) 
 
     entity->attachComponent(new SceneNodeComponent(playerNode));
     entity->attachComponent(new PositionComponent(x, y, z));
+    entity->attachComponent(new RotationComponent());
 
     return entity;
 }
