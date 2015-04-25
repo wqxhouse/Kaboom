d485bb43659f3b62707c66f8b059b6cdd50cecc4
Wai Ho Leung <whleung@live.com>
2015-04-25 04:32:36 -0700

    Fixed uppercase not moving issue.

--- KaboomClient/src/input/KeyboardEventHandler.cpp
+++ KaboomClient/src/input/KeyboardEventHandler.cpp
@@ -10,71 +10,77 @@ KeyboardEventHandler::~KeyboardEventHandler() {
 }
 
 bool KeyboardEventHandler::bindKey(int key, Function func) {
-    if (keyDownFuncMap.end() != keyDownFuncMap.find(key)) {
-        std::cout << "Duplicate key '" << key << "' ignored." << std::endl;
+    int lowerKey = tolower(key);
+
+    if (keyDownFuncMap.end() != keyDownFuncMap.find(lowerKey)) {
+        std::cout << "Duplicate key '" << lowerKey << "' ignored." << std::endl;
         return false;
     } else {
-        keyDownFuncMap[key].keyFunction = func;
+        keyDownFuncMap[lowerKey].keyFunction = func;
         return true;
     }
 }
 
 bool KeyboardEventHandler::bindKey(int key, KeyState state, Function func) {
+    int lowerKey = tolower(key);
+
     if (state == KEY_DOWN) {
-        return bindKey(key, func);
+        return bindKey(lowerKey, func);
     } else {
-        if (keyUpFuncMap.end() != keyUpFuncMap.find(key)) {
-            std::cout << "Duplicate key '" << key << "' ignored." << std::endl;
+        if (keyUpFuncMap.end() != keyUpFuncMap.find(lowerKey)) {
+            std::cout << "Duplicate key '" << lowerKey << "' ignored." << std::endl;
             return false;
         } else {
-            keyUpFuncMap[key].keyFunction = func;
+            keyUpFuncMap[lowerKey].keyFunction = func;
             return true;
         }
     }
 }
 
 bool KeyboardEventHandler::handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &aa) {
-	
-    KeyFunctionMap::iterator itr;
-    bool newKeyDownEvent = false;
-    bool newKeyUpEvent = false;
+    int key = tolower(ea.getKey());
 
     switch (ea.getEventType()) {
-    case osgGA::GUIEventAdapter::KEYDOWN:
-        itr = keyDownFuncMap.find(ea.getKey());
-        
-        if (itr != keyDownFuncMap.end()) {
-            if (itr->second.keyState == KEY_UP) {
-                itr->second.keyState = KEY_DOWN;
-                newKeyDownEvent = true;
+        case osgGA::GUIEventAdapter::KEYDOWN: {
+            bool newKeyDownEvent = false;
+            auto itr = keyDownFuncMap.find(key);
+
+            if (itr != keyDownFuncMap.end()) {
+                if (itr->second.keyState == KEY_UP) {
+                    itr->second.keyState = KEY_DOWN;
+                    newKeyDownEvent = true;
+                }
+
+                if (newKeyDownEvent) {
+                    (inputEventHandler.*(itr->second.keyFunction))();
+                    newKeyDownEvent = false;
+                }
+
+                return true;
             }
 
-            if (newKeyDownEvent) {
+            return false;
+        }
+        case osgGA::GUIEventAdapter::KEYUP: {
+            bool newKeyUpEvent = false;
+            auto itr = keyDownFuncMap.find(key);
+
+            if (itr != keyDownFuncMap.end()) {
+                itr->second.keyState = KEY_UP;
+            }
+
+            itr = keyUpFuncMap.find(key);
+
+            if (itr != keyUpFuncMap.end()) {
                 (inputEventHandler.*(itr->second.keyFunction))();
-                newKeyDownEvent = false;
+                return true;
             }
 
-            return true;
+            return false;
         }
-
-        return false;
-    case osgGA::GUIEventAdapter::KEYUP:
-        itr = keyDownFuncMap.find(ea.getKey());
-
-        if (itr != keyDownFuncMap.end()) {
-            itr->second.keyState = KEY_UP;
+        default: {
+            return false;
         }
-
-        itr = keyUpFuncMap.find(ea.getKey());
-
-        if (itr != keyUpFuncMap.end()) {
-            (inputEventHandler.*(itr->second.keyFunction))();
-            return true;
-        }
-
-        return false;
-    default:
-        return false;
     }
 }
 
