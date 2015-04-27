d2ac685793593e14a9154e3fcfe0043fd3aaf65d
Steven Leung <stevenleung002@Gmail.com>
2015-04-26 20:12:49 -0700

    Added 4 walls and collision detect (Quick Hack)
    Need to move this to the xml file instead

--- KaboomClient/src/Scene.cpp
+++ KaboomClient/src/Scene.cpp
@@ -8,7 +8,7 @@
 #include "MaterialManager.h"
 
 #include <osgDB/ReadFile>
-
+#include <GeometryObject.h>
 void setupScene()
 {
 	World &w = Core::getWorldRef();
@@ -32,19 +32,19 @@ void setupScene()
 	osg::Vec3 sunsetColor = osg::Vec3(0.9, 0.8, 0.5);
 	l->addDirectionalLight("Sun", osg::Vec3(-1, -1, -1), sunsetColor, false);
 
-	Core::setEnvironmentMapVerticalCross("C:/Users/Brian/Documents/GitHub/Kaboom/Media/haha.png");
+	Core::setEnvironmentMapVerticalCross("C:/Users/w3leung/desktop/Kaboom/Media/haha.png");
 
 	//Core::setEnvironmentMap(
 	//	"Cubemap_snow/posx.jpg", "Cubemap_snow/negx.jpg",
 	//	"Cubemap_snow/posy.jpg", "Cubemap_snow/negy.jpg",
 	//	"Cubemap_snow/posz.jpg", "Cubemap_snow/negz.jpg");
 	Core::setEnvironmentMap(
-		"cubemaps/ice/posx.jpg",
-		"cubemaps/ice/negx.jpg",
-		"cubemaps/ice/posy.jpg",
-		"cubemaps/ice/negy.jpg",
-		"cubemaps/ice/posz.jpg",
-		"cubemaps/ice/negz.jpg");
+		"Map/posx.png",
+		"Map/negx.png",
+		"Map/posy.png",
+		"Map/negy.png",
+		"Map/posz.png",
+		"Map/negz.png");
 
 	//Core::setEnvironmentMap(
 	//	"cubemaps/uffizi0000.tif",
@@ -65,8 +65,8 @@ void setupScene()
 	
 	osg::Node *loadedCow = osgDB::readNodeFile("Mesh/sphere.dae");
 
-	g->addGeometry("cow", loadedCow, osg::Vec3(-1, 0, 0));
-	g->addGeometry("second", loadedCow, osg::Vec3(1, 0, 0));
+	g->addGeometry("cow", loadedCow, osg::Vec3(0, 0, 0));
+	//g->addGeometry("second", loadedCow, osg::Vec3(1, 0, 0));
 	m->createPlainMaterial("planeMat", osg::Vec3(0.8, 0.3, 0.3), 0.5, 0.5, 0.5);
 	g->setGeometryMaterial("cow", m->getMaterial("planeMat"));
 	g->setGeometryMaterial("second", m->getMaterial("planeMat"));
@@ -81,4 +81,71 @@ void setupScene()
 
 	osg::Vec3 colorBLUE = osg::Vec3(0.3, 0.3, 0.7);
 	l->addPointLight("pt4", osg::Vec3(0, -7, 0), colorBLUE, 100, false);
+
+
+	//osg::ref_ptr<osg::Box> floor = new osg::Box;
+	//osg::ref_ptr<osg::ShapeDrawable> drawable = new osg::ShapeDrawable(floor);
+	osg::ref_ptr<osg::Geode> floorGeo = new osg::Geode;
+	//floorModel->addDrawable(drawable);
+	
+	//g->addGeometry("floor", floorModel, osg::Vec3(0,0,0));
+	
+
+	osg::ref_ptr<osg::Vec3Array> floorVertices = new osg::Vec3Array;
+	floorVertices->push_back(osg::Vec3(-10, -10, 0)); //left front
+	floorVertices->push_back(osg::Vec3(10, -10, 0));  //right front
+	floorVertices->push_back(osg::Vec3(10, 10, 0));   //right back
+	floorVertices->push_back(osg::Vec3(-10, 10, 0));  //left back
+	
+	osg::ref_ptr<osg::Geometry> floorGeometry = new osg::Geometry;
+	floorGeometry->setVertexArray(floorVertices);
+
+	osg::ref_ptr<osg::DrawElementsUInt> floorBase = new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
+	floorBase->push_back(0);
+	floorBase->push_back(1);	
+	floorBase->push_back(2);
+	floorBase->push_back(3);
+	floorGeometry->addPrimitiveSet(floorBase);
+
+	floorGeo->addDrawable(floorGeometry);
+	g->addGeometry("floor", floorGeo, osg::Vec3(0, 0, 0));
+	g->setGeometryMaterial("floor", m->getMaterial("planeMat"));
+
+
+	osg::ref_ptr<osg::Geode> wallBack = new osg::Geode(*floorGeo, osg::CopyOp::DEEP_COPY_ALL);
+	g->addGeometry("wallBack", wallBack, osg::Vec3(0, -10, 10));
+	g->setGeometryMaterial("wallBack", m->getMaterial("planeMat"));
+	osg::Quat q0;
+	q0.makeRotate(osg::DegreesToRadians(90.0), -1, 0, 0);
+	g->getGeometryObject("wallBack")->setRotation(q0);
+
+
+
+	osg::ref_ptr<osg::Geode> wallFront = new osg::Geode(*floorGeo, osg::CopyOp::DEEP_COPY_ALL);
+	g->setGeometryMaterial("wallFront", m->getMaterial("planeMat"));
+	g->addGeometry("wallFront", wallFront, osg::Vec3(0, 10, 10));
+	g->setGeometryMaterial("wallFront", m->getMaterial("planeMat"));
+	osg::Quat q1;
+	q1.makeRotate(osg::DegreesToRadians(90.0), 1, 0, 0);
+	g->getGeometryObject("wallFront")->setRotation(q1);
+
+
+	osg::ref_ptr<osg::Geode> wallLeft = new osg::Geode(*floorGeo, osg::CopyOp::DEEP_COPY_ALL);
+	g->setGeometryMaterial("wallLeft", m->getMaterial("planeMat"));
+	g->addGeometry("wallLeft", wallLeft, osg::Vec3(-10, 0, 10));
+	//g->setGeometryMaterial("wallLeft", m->getMaterial("planeMat"));
+	osg::Quat q2;
+	q2.makeRotate(osg::DegreesToRadians(90.0), 0, 1, 0);
+	g->getGeometryObject("wallLeft")->setRotation(q2);
+
+	osg::ref_ptr<osg::Geode> wallRight = new osg::Geode(*floorGeo, osg::CopyOp::DEEP_COPY_ALL);
+	g->setGeometryMaterial("wallRight", m->getMaterial("planeMat"));
+	g->addGeometry("wallRight", wallRight, osg::Vec3(10, 0, 10));
+	//g->setGeometryMaterial("wallRight", m->getMaterial("planeMat"));
+	osg::Quat q3;
+	q3.makeRotate(osg::DegreesToRadians(90.0), 0, -1, 0);
+	g->getGeometryObject("wallRight")->setRotation(q3);
+
+	//g->getGeometryObject("floor2")->setTranslate(q0);
+
 }
