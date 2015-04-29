08db5cbc13a73418cb4d4a27d664cf2a4ed2756f
wqxhouse <wqxhouse1@gmail.com>
2015-04-28 23:03:45 -0700

    Modified cubemap utility so that it can read floating point images; used to support HDR

--- KaboomGraphicsEngine/include/CubemapUtil.h
+++ KaboomGraphicsEngine/include/CubemapUtil.h
@@ -16,7 +16,34 @@ public:
 
 private:
 	void allocCubeImages(int singleImageDim);
+
+	struct pixelStructRGB {
+		unsigned char r, g, b;
+	};
+
+	struct pixelStructRGB16 {
+		short r, g, b;
+	};
+
+	struct pixelStructRGB32 {
+		float r, g, b;
+	};
+
+	struct pixelStructRGBA {
+		unsigned char r, g, b, a;
+	};
+
+	struct pixelStructRGBA16 {
+		short r, g, b, a;
+	};
+
+	struct pixelStructRGBA32 {
+		float r, g, b, a;
+	};
 	
 	std::vector<osg::ref_ptr<osg::Image> > _cubeMapImages;
 	osg::ref_ptr<osg::Image> createSubImage(osg::Image *sourceImage, int startX, int startY, int stopX, int stopY, bool rotate);
+	void copyPixel(GLenum internalFormat, unsigned char *srcBuffer, unsigned char *dstBuffer,
+		int currX, int currY, int subX, int subY, int subImgWidth, int subImgHeight, int srcImgWidth, bool rotate);
+	unsigned char *createImageBuffer(GLenum type, int subImageWidth, int subImageHeight);
 };
