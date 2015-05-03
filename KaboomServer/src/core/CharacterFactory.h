6a8180ebafd00761122be38bdded56336565a613
Wai Ho Leung <whleung@live.com>
2015-05-03 00:38:28 -0700

    Added parameters to CharacterFactory to make it more configurable.

--- KaboomServer/src/core/CharacterFactory.h
+++ KaboomServer/src/core/CharacterFactory.h
@@ -1,13 +1,20 @@
 #pragma once
 
 class Entity;
+enum EntityType;
 class EntityManager;
 
 class CharacterFactory {
 public:
     CharacterFactory(EntityManager &entityManager);
 
-    Entity *createCharacter(float x = 0.0f, float y = 0.0f, float z = 0.0f) const;
+    Entity *createCharacter(
+            const EntityType &characterType,
+            float x = 0.0f,
+            float y = 0.0f,
+            float z = 0.0f,
+            float yaw = 0.0f,
+            float pitch = 0.0f) const;
 
 private:
     EntityManager &entityManager;
