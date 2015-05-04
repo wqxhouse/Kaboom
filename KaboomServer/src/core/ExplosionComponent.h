0f9b32352377d4ebe540885dad4f4871aa13445d
Wai Ho Leung <whleung@live.com>
2015-05-04 04:57:59 -0700

    Extracted explosion logic into ExplosionHandler.

--- KaboomServer/src/core/ExplosionComponent.h
+++ KaboomServer/src/core/ExplosionComponent.h
@@ -2,5 +2,14 @@
 
 #include <core/Component.h>
 
+#include "ExplosionHandler.h"
+
 class ExplosionComponent : public Component {
+public:
+    ExplosionComponent(ExplosionHandler *handler);
+
+    ExplosionHandler *getHandler() const;
+
+private:
+    ExplosionHandler *handler;
 };
--- KaboomServer/src/core/KaboomV2CollisionHandler.cpp
+++ KaboomServer/src/core/KaboomV2CollisionHandler.cpp
@@ -3,10 +3,11 @@
 #include <core/Entity.h>
 
 #include "ExplosionComponent.h"
+#include "KaboomV2ExplosionHandler.h"
 
 void KaboomV2CollisionHandler::handle(
         Game *game,
         Entity *entity,
         const std::unordered_set<Entity *> &collidedEntities) {
-    entity->attachComponent(new ExplosionComponent());
+    entity->attachComponent(new ExplosionComponent(new KaboomV2ExplosionHandler));
 }
