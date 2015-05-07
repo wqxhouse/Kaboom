ff9bf9dadfb0767a18d4a7f7c464af069f03f596
Wai Ho Leung <whleung@live.com>
2015-05-07 16:32:38 -0700

    Renamed math/Conversion to math/util.
    If this file grows big, we can split it into different files.

--- KaboomServer/src/systems/FiringSystem.cpp
+++ KaboomServer/src/systems/FiringSystem.cpp
@@ -11,7 +11,7 @@
 #include "../components/InputComponent.h"
 #include "../core/EntityConfigLookup.h"
 #include "../core/Game.h"
-#include "../math/Conversion.h"
+#include "../math/util.h"
 
 #define VELOCITYCAP 2
 #define VELOCTIYACCELERATION .1
