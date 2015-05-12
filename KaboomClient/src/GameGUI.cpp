37090d1afb8a273289d0caa84944f3f155d7b3f8
wqxhouse <wqxhouse1@gmail.com>
2015-05-12 00:07:29 -0700

    change geometry file copy path; change in game gui to marty.rml

--- KaboomClient/src/GameGUI.cpp
+++ KaboomClient/src/GameGUI.cpp
@@ -12,27 +12,28 @@ void setupGUI()
 	ConfigSettings::config->getValue("MediaPath", mediaPath);
 
 	std::string guiPath = mediaPath + "DefaultAssets\\LibRocketGUI\\InGame\\";
-	int window_id = manager->addWindow(guiPath + "demo.rml", true);
+	/*int window_id = manager->addWindow(guiPath + "demo.rml", true);*/
 
+	manager->addWindow(guiPath + "marty.rml", true);
 	/*int i = _libRocketInGameGUI->addWindow(guiPath + "InGame\\marty.rml", true);
 	_libRocketInGameGUI->getWindow(i - 1)->Hide();
 	_libRocketInGameGUI->getWindow(i)->Hide();*/
 
 	// get the window
-	Rocket::Core::ElementDocument *window1 = manager->getWindow(window_id);
-	Rocket::Core::Element * win = window1->GetFirstChild();
-	Rocket::Core::Element * newElement = window1->CreateElement("div");
-	if (newElement == NULL)
-	{
-		return;
-	}
-	Rocket::Core::ElementText * new_text_element = window1->CreateTextNode("Sample Marty");
-	if (new_text_element == NULL)
-	{
-		newElement->RemoveReference();
-		return;
-	}
-	//AppendChild->newElement(new_text_element);
-	//win->AppendChild(newElement);
-	std::cout << "after: " << win->GetNumChildren() << std::endl;
+	//Rocket::Core::ElementDocument *window1 = manager->getWindow(window_id);
+	//Rocket::Core::Element * win = window1->GetFirstChild();
+	//Rocket::Core::Element * newElement = window1->CreateElement("div");
+	//if (newElement == NULL)
+	//{
+	//	return;
+	//}
+	//Rocket::Core::ElementText * new_text_element = window1->CreateTextNode("Sample Marty");
+	//if (new_text_element == NULL)
+	//{
+	//	newElement->RemoveReference();
+	//	return;
+	//}
+	////AppendChild->newElement(new_text_element);
+	////win->AppendChild(newElement);
+	//std::cout << "after: " << win->GetNumChildren() << std::endl;
 }
