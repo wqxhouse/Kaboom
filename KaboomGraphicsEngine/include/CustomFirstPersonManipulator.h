c5618764b8f159467b55d95ca9b9df66761896de
wqxhouse <wqxhouse1@gmail.com>
2015-05-13 02:54:27 -0700

    When dragging the fps camera in the editor while mouse over the gui, the camera will not lock, making the editor more usable

--- KaboomGraphicsEngine/include/CustomFirstPersonManipulator.h
+++ KaboomGraphicsEngine/include/CustomFirstPersonManipulator.h
@@ -21,17 +21,24 @@ public:
 
 	void setWalkingSpeed(float metersPerSec);
 	float getWalkingSpeed();
+	bool isDragging();
 
 protected:
 	virtual bool handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &us);
 	virtual bool handleFrame(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &us);
 	virtual bool handleKeyDown(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &us);
 	virtual bool handleKeyUp(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &us);
+	virtual bool handleMouseDrag(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us);
+	virtual bool handleMouseRelease(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us);
+
+	void clearMovement();
 
 	bool _movingForward;
 	bool _movingBackward;
 	bool _movingLeft;
 	bool _movingRight;
+
+	bool _isDragging;
 
 	float _metersPerSec;
 };
