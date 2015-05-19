6773f9354d991dcb5b22e3569b9974ccd3142a22
stevenleung002 <w3leung@ACS-CSEB220-06.ad.ucsd.edu>
2015-05-18 19:31:49 -0700

    Added the Map and the models, more spawn points.

--- KaboomClient/src/Scene.cpp
+++ KaboomClient/src/Scene.cpp
@@ -91,12 +91,12 @@ void setupScene()
 	createInfinitePlane(g, m);
 	/*l->addDirectionalLight("Sun", osg::Vec3(0.1, -0.88, -0.46), osg::Vec3(0.7, 0.7, 0.7), false);*/
 
-	osg::ref_ptr<osg::Sphere> sp = new osg::Sphere;
-	osg::ref_ptr<osg::ShapeDrawable> sd = new osg::ShapeDrawable;
-	sd->setShape(sp);
-	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-	geode->addDrawable(sd);
-	g->addGeometry("sphere", geode, osg::Vec3(0, 20, 1));
+	//osg::ref_ptr<osg::Sphere> sp = new osg::Sphere;
+	//osg::ref_ptr<osg::ShapeDrawable> sd = new osg::ShapeDrawable;
+	//sd->setShape(sp);
+	//osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	//geode->addDrawable(sd);
+	//g->addGeometry("sphere", geode, osg::Vec3(0, 20, 1));
 	/*
 	m->createPlainMaterial("SphereMat", osg::Vec3(0.3, 0.3, 0.3), 0.1, 0.5, 1.0);
 	g->setGeometryMaterial("sphere", m->getMaterial("SphereMat"));
