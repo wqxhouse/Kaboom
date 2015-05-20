148adaa236309b1a5e12f3eab1ec67851a24d0f3
Wai Ho Leung <whleung@live.com>
2015-05-20 03:29:35 -0700

    Added functionality for firing 3 time bombs at once.

--- KaboomServer/src/math/util.h
+++ KaboomServer/src/math/util.h
@@ -9,6 +9,8 @@ Quat euler2Quat(float yaw, float pitch, float roll);
 
 btVector3 getViewDirection(Quat rotation);
 
+Vec3 rotateVector(const Vec3 &v, const Vec3 &axis, float deg);
+
 btVector3 getImpulseVector(btVector3 pointA, btVector3 pointB, btScalar knockBackRatio);
 
 float getDistance(const Vec3 &pos1, const Vec3 &pos2);
